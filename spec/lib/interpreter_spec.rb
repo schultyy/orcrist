53e52e1a0b93bb26d51c638c2bb84417b27c5a85
Jan Schulte <hello@unexpected-co.de>
2014-07-12 00:33:09 +0200

    Add assignment specs

--- spec/lib/interpreter_spec.rb
+++ spec/lib/interpreter_spec.rb
@@ -9,4 +9,23 @@ describe 'Interpreter' do
   it 'interprets number' do
     expect(@interpreter.eval('5').ruby_value).to eq(5)
   end
+
+  describe 'assignment' do
+    before do
+      @interpreter.eval('a = 5')
+    end
+    context 'context' do
+      it 'context has a local' do
+        expect(RootContext.locals.length).to eq 1
+      end
+
+      it 'has local with ID "a"' do
+        expect(RootContext.locals.fetch('a')).to_not be_nil
+      end
+
+      it 'ID "a" has value 5' do
+        expect(RootContext.locals.fetch('a').ruby_value).to eq 5
+      end
+    end
+  end
 end
